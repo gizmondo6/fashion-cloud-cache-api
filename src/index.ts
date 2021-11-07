a8ca0203ba65ac4e073681466d507d224bfc0084
gizmondo6 <gizmondo.anonymous@gmail.com>
2021-11-07 16:05:38 +0300

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-import http from 'http'
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-console.log('Started')
-http.createServer().listen(3000)
+dotenv.config();
+
+if (!process.env.REST_API_PORT) {
+  console.error("Please provide REST API port as REST_API_PORT environment variable")
+  process.exit(1);
+}
+
+const REST_API_PORT: number = parseInt(process.env.REST_API_PORT as string, 10);
+
+const app = express();
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(REST_API_PORT, () => {
+  console.log(`REST API is listening on port ${REST_API_PORT}`);
+});
